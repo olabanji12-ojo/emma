acba817029206be1e8adec82688bc8b69864cb24
olabanji ojo <emmanuelojo291@gmail.com>
2023-08-22 05:47:50 -0700

    modified the code

--- name.py
+++ name.py
@@ -1,1 +1,17 @@
-print("hello world")
+print("guessing game")
+import random
+random_number = random.randint(1, 10)
+guess = 0
+while True:
+    guess += 1
+    guesses = int(input("Guess the number?: "))
+    if guesses == random_number:
+        print("You guessed the number correctly in ", guess, "guesses")
+        break
+    elif guesses > random_number:
+        print("Try again, too high.")
+    elif guesses < random_number:
+        print("Try again, too low.")
+
+
+
