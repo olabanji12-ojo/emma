f8e92456d092b3ae911ceac3c5c9a60f770af648
olabanji ojo <emmanuelojo291@gmail.com>
2023-08-22 05:55:24 -0700

    computer guessing random number

---
+++ randomnumber.py
@@ -0,0 +1,15 @@
+import random
+a = int(input("enter the lower boundary?: "))
+b = int(input("enter the upper boundary?: "))
+
+while True: 
+    random_number = random.randint(a, b)
+    feedback = input(f"is {random_number} too high, too low or correct?: ")
+    if feedback == "h":
+        b = random_number - 1
+    elif feedback == "l":
+        a = random_number + 1
+    elif feedback == "c":
+        print("the computer guessed the number correctly!")
+        break
+
